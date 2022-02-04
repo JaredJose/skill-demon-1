6363340527a78bc3716c0999d6b717e7657564fd
JaredJose <jjose@ucsd.edu>
2022-02-03 22:33:33 -0800

    Created example java file and failing JUnit test

---
+++ ExampleTest.java
@@ -0,0 +1,12 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class ExampleTest {
+   @Test
+   public void testAdder(){
+       int num1 = 1;
+       int num2 = 2;
+       assertEquals(2, Example.adder(num1,num2));
+   } 
+}
